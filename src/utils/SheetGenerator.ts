851e9b85d36b7e769bc8c6f1e7853370a87c9800
Raikuro <jugragu22@gmail.com>
2020-01-26 18:12:32 +0100

    Implemented methods to generate characteristics

--- src/utils/SheetGenerator.ts
+++ src/utils/SheetGenerator.ts
@@ -1,6 +1,49 @@
-export abstract class SheetGenerator{
-    public static readonly generateCharacteristicsType1 = () => undefined
-    public static readonly generateCharacteristicsType2 = () => undefined
-    public static readonly generateCharacteristicsType3 = () => undefined
-    public static readonly generateCharacteristicsType4 = () => undefined
+export abstract class SheetGenerator {
+    public static readonly generateCharacteristicsType1: () => number[] = () => {
+        let values: number[] = undefined;
+        let actual: number
+        let minimunValueIndex = 0;
+        for (let i = 0; i < 8; i++) {
+            do {
+                actual = SheetGenerator.throw1d10();
+            }
+            while (actual < 4)
+            if (actual < values[minimunValueIndex]) {
+                minimunValueIndex = i
+            }
+            values.push(actual);
+        }
+        values[minimunValueIndex] = 9;
+        return values;
+    }
+
+    public static readonly generateCharacteristicsType2: () => number[] = () => {
+        let values: number[] = undefined;
+        for (let i = 0; i < 8; i++) {
+            let first = SheetGenerator.throw1d10();
+            let second = SheetGenerator.throw1d10();
+            values.push(first > second ? first : second);
+        }
+        return values;
+    }
+
+    public static readonly generateCharacteristicsType3: () => number[] = () => {
+        let values: number[] = undefined;
+        for (let i = 0; i < 8; i++) {
+            values.push(SheetGenerator.throw1d10());
+        }
+        return values;
+    }
+
+    public static readonly generateCharacteristicsType4: () => number = () => {
+        let sum: number = 0;
+        for (let i = 0; i < 7; i++) {
+            sum += SheetGenerator.throw1d10();
+        }
+        return sum;
+    }
+
+    private static throw1d10: () => number = () => {
+        return Math.floor(Math.random() * 10) + 1;
+    }
 }
